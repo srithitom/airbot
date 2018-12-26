2343c991da4b69fbabfc8848d5ac2f472fbd3e13
MagenTa <t_o_tar_dexza@windowslive.com>
2018-12-26 21:35:26 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,2 +1,10 @@
 <?php
-echo "OKtest";
+$conn_string = "host=ec2-54-243-212-227.compute-1.amazonaws.com port=5432 dbname=derp1q2mqmgk73 user=qhfmxvhvaduhkw pass=90b8104676e32efd9c1d98b72f3aba4c314ce0b9d6e82234f6c9fb56e6c60bd2";
+$dbconn = pg_connect($conn_string);
+
+$result=pg_query("SELECT * FROM appointments");
+
+$row1=pg_fetch_array($result);
+echo $row1;
+
+pg_close($dbconn)
