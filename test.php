04f849126d3083c888f1250f040a165b9c94fb5f
MagenTa <t_o_tar_dexza@windowslive.com>
2018-12-26 21:39:02 +0700

    Update test.php

--- test.php
+++ test.php
@@ -1,10 +1,26 @@
 <?php
-$conn_string = "host=ec2-54-243-212-227.compute-1.amazonaws.com port=5432 dbname=derp1q2mqmgk73 user=qhfmxvhvaduhkw pass=90b8104676e32efd9c1d98b72f3aba4c314ce0b9d6e82234f6c9fb56e6c60bd2";
-$dbconn = pg_connect($conn_string);
+// Connecting, selecting database
+$dbconn = pg_connect("host=ec2-54-243-212-227.compute-1.amazonaws.com dbname=derp1q2mqmgk73 user=qhfmxvhvaduhkw password=90b8104676e32efd9c1d98b72f3aba4c314ce0b9d6e82234f6c9fb56e6c60bd2")
+    or die('Could not connect: ' . pg_last_error());
 
-$result=pg_query("SELECT * FROM appointments");
+// Performing SQL query
+$query = 'SELECT * FROM appointments';
+$result = pg_query($query) or die('Query failed: ' . pg_last_error());
 
-$row1=pg_fetch_array($result);
-echo $row1;
+// Printing results in HTML
+echo "<table>\n";
+while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
+    echo "\t<tr>\n";
+    foreach ($line as $col_value) {
+        echo "\t\t<td>$col_value</td>\n";
+    }
+    echo "\t</tr>\n";
+}
+echo "</table>\n";
 
-pg_close($dbconn)
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close($dbconn);
+?>
