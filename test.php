705255d34ad96a16dd3a55754be25d6d8e1d5eda
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-12 12:10:42 +0700

    Update test.php

--- test.php
+++ test.php
@@ -12,7 +12,7 @@ echo "<table>\n";
 while ($line = pg_fetch_array($result)) {
     echo '<pre>'; print_r($line); echo '</pre>';
     var_dump($line);
-    echo $line->time '<br>';
+    echo $line[2];
     foreach($line as $res) {
     echo $res, '<br />';
 }
