fde25f526fa6a1ba50cea4afe66a01439ff431d3
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-12 11:26:52 +0700

    Update test.php

--- test.php
+++ test.php
@@ -11,6 +11,7 @@ $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 echo "<table>\n";
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
     echo '<pre>'; print_r($line); echo '</pre>';
+    echo $line->time;
     echo "\t<tr>\n";
     foreach ($line as $col_value) {
         echo "\t\t<td>$col_value</td>\n";
