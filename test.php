592b75b598cec8e03b6367ae505ceda45d9d1344
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-12 11:12:25 +0700

    Update test.php

--- test.php
+++ test.php
@@ -10,7 +10,8 @@ $result = pg_query($query) or die('Query failed: ' . pg_last_error());
 // Printing results in HTML
 echo "<table>\n";
 while ($line = pg_fetch_array($result, null, PGSQL_ASSOC)) {
-    echo "$line";
+    $i=0;
+    echo "$line[$i]";
     echo "\t<tr>\n";
     foreach ($line as $col_value) {
         echo "\t\t<td>$col_value</td>\n";
