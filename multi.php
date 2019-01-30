10e12e8a2fd04de269b756fb1b49397606fcd28e
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-30 12:48:32 +0700

    Update multi.php

--- multi.php
+++ multi.php
@@ -1,15 +1,22 @@
-<table>
+<html>
+<head>
+    <title>แม่สูตรคูณ</title>
+    <meta charset="UTF-8">
+</head>
+<body>
+    <table>
     <?php
-$i = 1;
-$num = 41;
-echo "55543206041-9 number is 41 <br>";
-do {
-    <tr>
-        <th><?=echo "41 x " . $i;?></th>
-        <td><?=echo " = " . $num*$i;?></td>
-    </tr>
-    
-    $i += 1;
-} while ($i <= 12);
-?>
-</table>
+        $i = 1;
+        $num = 41;
+        echo "55543206041-9 number is 41 <br>";
+        do {
+            echo "<tr>
+                    <th>41 x $i</th>
+                    <td> = $num*$i</td>
+                   </tr>"
+            $i += 1;
+        } while ($i <= 12);
+    ?>
+    </table>
+</body>
+</html>
