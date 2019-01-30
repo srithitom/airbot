1a7b8a19d90cff4328ed396fc8711ecde7abf400
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-30 13:52:27 +0700

    Update multi.php

--- multi.php
+++ multi.php
@@ -2,11 +2,6 @@
 <head>
     <title>เกรด</title>
     <meta charset="UTF-8">
-    <style>
-        .bggreen { background-color:#00FF00; }
-        .bgred { background-color:#FF0000; }
-        .bgyellow { background-color:#FFFF00; }
-    </style>
 </head>
 <body>
     <center>
@@ -14,21 +9,21 @@
         echo "<b><u>LAB5.2 เกรด</b></u><br>";
         $score = 70;
         if($score >= 80){
-            echo "<p class"bggreen">$score คะแนน : ได้รับระดับเกรด A</p>";
+            echo "<p style="background-color: green">$score คะแนน : ได้รับระดับเกรด A</p>";
         }elseif($score >= 75){
-            echo "<p class"bggreen">$score คะแนน : ได้รับระดับเกรด B+</p>";
+            echo "<p style="background-color: green">$score คะแนน : ได้รับระดับเกรด B+</p>";
         }elseif($score >= 70){
-            echo "<p class"bggreen">$score คะแนน : ได้รับระดับเกรด B</p>";
+            echo "<p style="background-color: green">$score คะแนน : ได้รับระดับเกรด B</p>";
         }elseif($score >= 65){
-            echo "<p class"bgyellow">$score คะแนน : ได้รับระดับเกรด C+</p>";
+            echo "<p style="background-color: yellow">$score คะแนน : ได้รับระดับเกรด C+</p>";
         }elseif($score >= 60){
-            echo "<p class"bgyellow">$score คะแนน : ได้รับระดับเกรด C</p>";
+            echo "<p style="background-color: yellow">$score คะแนน : ได้รับระดับเกรด C</p>";
         }elseif($score >= 55){
-            echo "<p class"bgyellow">$score คะแนน : ได้รับระดับเกรด D+</p>";
+            echo "<p style="background-color: yellow">$score คะแนน : ได้รับระดับเกรด D+</p>";
         }elseif($score >= 50){
-            echo "<p class"bgyellow">$score คะแนน : ได้รับระดับเกรด D</p>";
+            echo "<p style="background-color: yellow">$score คะแนน : ได้รับระดับเกรด D</p>";
         }else{
-            echo "<p class"bgred">$score คะแนน : ได้รับระดับเกรด F</p>";
+            echo "<p style="background-color: red">$score คะแนน : ได้รับระดับเกรด F</p>";
         }
     ?>
     </center>
