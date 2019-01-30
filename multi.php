68b55eaf0e62deae1c6e0307d7f09d069b661970
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-30 14:43:03 +0700

    Update multi.php

--- multi.php
+++ multi.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     <center>
-    <?php
+    <p><?php
         echo "<b><u>LAB5.2 เกรด</b></u><br>";
         $score = 49;
         if($score >= 80 and $score <=100){
@@ -32,7 +32,7 @@
         }else{
             echo "คุณกรอกคะแนนผิด กรุณากรอกอีกครั้ง"
         }
-    ?><br><br>
+    ?></p><br><br>
     </center>
 </body>
 </html>
