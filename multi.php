8598acb60be0cf46c25b2fa47ba917cc81b6e8a2
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-30 12:29:22 +0700

    Update multi.php

--- multi.php
+++ multi.php
@@ -3,7 +3,12 @@ $i = 1;
 $num = 41;
 echo "55543206041-9 number is 41 <br>";
 do {
-    echo "41 x " . $i . " = " . $num*$i ."<br>";
+    <table>
+    <tr>
+        <th>echo "41 x " . $i;</th>
+        <td>echo " = " . $num*$i;</td>
+    </tr>
+    </table>
     $i += 1;
 } while ($i <= 12);
 ?>
