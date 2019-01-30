8effeafb4bc2d4b233abef6a1e67b24d6a555515
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-30 14:51:50 +0700

    Update multi.php

--- multi.php
+++ multi.php
@@ -31,6 +31,26 @@
             echo "<span class=\"bgred\">$score คะแนน : ได้รับระดับเกรด F</span>";
         }
     ?></p><br><br>
+    <table>
+    <tr><th>ระดับคะแนน (เกรด)</th><th>คะแนน</th>
+    </tr>
+    <tr bgcolor="#00FF00"><th>A</th><td>80 คะแนนขึ้นไป</td>
+    </tr>
+    <tr bgcolor="#00FF00"><th>B+</th><td>75 – 79 คะแนน</td>
+    </tr>
+    <tr bgcolor="#00FF00"><th>B</th><td>70 – 74 คะแนน</td>
+    </tr>
+    <tr bgcolor="#FFFF00"><th>C+</th><td>65 – 69 คะแนน</td>
+    </tr>
+    <tr bgcolor="#FFFF00"><th>C</th><td>60 – 64 คะแนน</td>
+    </tr>
+    <tr bgcolor="#FFFF00"><th>D</th><td>55 – 59 คะแนน</td>
+    </tr>
+    <tr bgcolor="#FFFF00"><th>C+</th><td>50 – 54 คะแนน</td>
+    </tr>
+    <tr bgcolor="#FF0000"><th>C+</th><td>0 – 49 คะแนน</td>
+    </tr>
+    </table>
     </center>
 </body>
 </html>
