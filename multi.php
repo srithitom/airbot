0714947a59513e994dbf080a5249ac001736acb3
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-30 15:23:34 +0700

    Update multi.php

--- multi.php
+++ multi.php
@@ -1,73 +1,24 @@
 <html>
 <head>
-    <title>เกรด</title>
+    <title>แม่สูตรคูณ</title>
     <meta charset="UTF-8">
-    <style>
-        .bggreen { background-color:#00FF00; }
-        .bgred { background-color:#FF0000; }
-        .bgyellow { background-color:#FFFF00; }
-        th, td {
-        border: 1px solid black;
-        padding: 5px;
-        text-align: center;
-        }
-    </style>
 </head>
 <body>
     <center>
-    <p><?php
-        echo "<b><u>LAB5.2 เกรด</b></u><br>";
-        $score = 69;
-        if($score > 100){
-            echo "คะแนนเกิน 100 กรุณากรอกคะแนนอีกครั้ง";
-        }elseif($score >= 80){
-            echo "<span class=\"bggreen\">$score คะแนน : ได้รับระดับเกรด A</span>";
-        }elseif($score >= 75){
-            echo "<span class=\"bggreen\">$score คะแนน : ได้รับระดับเกรด B+</span>";
-        }elseif($score >= 70){
-            echo "<span class=\"bggreen\">$score คะแนน : ได้รับระดับเกรด B</span>";
-        }elseif($score >= 65){
-            echo "<span class=\"bgyellow\">$score คะแนน : ได้รับระดับเกรด C+</span>";
-        }elseif($score >= 60){
-            echo "<span class=\"bgyellow\">$score คะแนน : ได้รับระดับเกรด C</span>";
-        }elseif($score >= 55){
-            echo "<span class=\"bgyellow\">$score คะแนน : ได้รับระดับเกรด D+</span>";
-        }elseif($score >= 50){
-            echo "<span class=\"bgyellow\">$score คะแนน : ได้รับระดับเกรด D</span>";
-        }elseif($score >= 0){
-            echo "<span class=\"bgred\">$score คะแนน : ได้รับระดับเกรด F</span>";
-        }else{
-            echo "คะแนนต่ำกว่า 0 กรุณากรอกคะแนนอีกครั้ง";
-        }
-    ?></p><br>
     <table>
-        <tr>
-            <th>ระดับคะแนน (เกรด)</th><th>คะแนน</th>
-        </tr>
-        <tr class="bggreen">
-            <th>A</th><td>80 คะแนนขึ้นไป</td>
-        </tr>
-        <tr class="bggreen">
-            <th>B+</th><td>75 – 79 คะแนน</td>
-        </tr>
-        <tr class="bggreen">
-            <th>B</th><td>70 – 74 คะแนน</td>
-        </tr>
-        <tr class="bgyellow">
-            <th>C+</th><td>65 – 69 คะแนน</td>
-        </tr>
-        <tr class="bgyellow">
-            <th>C</th><td>60 – 64 คะแนน</td>
-        </tr>
-        <tr class="bgyellow">
-            <th>D</th><td>55 – 59 คะแนน</td>
-        </tr>
-        <tr class="bgyellow">
-            <th>C+</th><td>50 – 54 คะแนน</td>
-        </tr>
-        <tr class="bgred">
-            <th>C+</th><td>0 – 49 คะแนน</td>
-        </tr>
+    <?php
+        $i = 1;
+        $num = 41;
+        echo "รหัสนักศึกษา 55543206041-9<br><b><u>LAB5.1 สูตรคูณแม่ 41 </b></u><br>";
+        do {
+            $mul=$num*$i;
+            echo "<tr>
+                    <td>41 x $i</td>
+                    <td> = $mul</td>
+                   </tr>";
+            $i += 1;
+        } while ($i <= 12);
+    ?>
     </table>
     </center>
 </body>
