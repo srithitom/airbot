e65aa7eac941ebe225a722a453d213febb0ed82a
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-30 14:00:55 +0700

    Update multi.php

--- multi.php
+++ multi.php
@@ -9,21 +9,21 @@
         echo "<b><u>LAB5.2 เกรด</b></u><br>";
         $score = 70;
         if($score >= 80){
-            echo "<p style="background-color: green;">$score คะแนน : ได้รับระดับเกรด A</p>";
+            echo "<p style=\"background-color: green;\">$score คะแนน : ได้รับระดับเกรด A</p>";
         }elseif($score >= 75){
-            echo "<p style="background-color: green;">$score คะแนน : ได้รับระดับเกรด B+</p>";
+            echo "<p style=\"background-color: green;\">$score คะแนน : ได้รับระดับเกรด B+</p>";
         }elseif($score >= 70){
-            echo "<p style="background-color: green;">$score คะแนน : ได้รับระดับเกรด B</p>";
+            echo "<p style=\"background-color: green;\">$score คะแนน : ได้รับระดับเกรด B</p>";
         }elseif($score >= 65){
-            echo "<p style="background-color: yellow;">$score คะแนน : ได้รับระดับเกรด C+</p>";
+            echo "<p style=\"background-color: yellow;\">$score คะแนน : ได้รับระดับเกรด C+</p>";
         }elseif($score >= 60){
-            echo "<p style="background-color: yellow;">$score คะแนน : ได้รับระดับเกรด C</p>";
+            echo "<p style=\"background-color: yellow;\">$score คะแนน : ได้รับระดับเกรด C</p>";
         }elseif($score >= 55){
-            echo "<p style="background-color: yellow;">$score คะแนน : ได้รับระดับเกรด D+</p>";
+            echo "<p style=\"background-color: yellow;\">$score คะแนน : ได้รับระดับเกรด D+</p>";
         }elseif($score >= 50){
-            echo "<p style="background-color: yellow;">$score คะแนน : ได้รับระดับเกรด D</p>";
+            echo "<p style=\"background-color: yellow;\">$score คะแนน : ได้รับระดับเกรด D</p>";
         }else{
-            echo "<p style="background-color: red;">$score คะแนน : ได้รับระดับเกรด F</p>";
+            echo "<p style=\"background-color: red;\">$score คะแนน : ได้รับระดับเกรด F</p>";
         }
     ?>
     </center>
