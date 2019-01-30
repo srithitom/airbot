ee0b379685e8054ece81b93cfe45af79e106e796
MagenTa <t_o_tar_dexza@windowslive.com>
2019-01-30 14:41:44 +0700

    Update multi.php

--- multi.php
+++ multi.php
@@ -10,7 +10,7 @@
 </head>
 <body>
     <center>
-    <div><?php
+    <?php
         echo "<b><u>LAB5.2 เกรด</b></u><br>";
         $score = 49;
         if($score >= 80 and $score <=100){
@@ -32,27 +32,7 @@
         }else{
             echo "คุณกรอกคะแนนผิด กรุณากรอกอีกครั้ง"
         }
-    ?></div><br><br>
-    <table>
-    <tr><th>ระดับคะแนน (เกรด)</th><th>คะแนน</th>
-    </tr>
-    <tr bgcolor="#00FF00"><th>A</th><td>80 คะแนนขึ้นไป</td>
-    </tr>
-    <tr bgcolor="#00FF00"><th>B+</th><td>75 – 79 คะแนน</td>
-    </tr>
-    <tr bgcolor="#00FF00"><th>B</th><td>70 – 74 คะแนน</td>
-    </tr>
-    <tr bgcolor="#FFFF00"><th>C+</th><td>65 – 69 คะแนน</td>
-    </tr>
-    <tr bgcolor="#FFFF00"><th>C</th><td>60 – 64 คะแนน</td>
-    </tr>
-    <tr bgcolor="#FFFF00"><th>D</th><td>55 – 59 คะแนน</td>
-    </tr>
-    <tr bgcolor="#FFFF00"><th>C+</th><td>50 – 54 คะแนน</td>
-    </tr>
-    <tr bgcolor="#FF0000"><th>C+</th><td>0 – 49 คะแนน</td>
-    </tr>
-    </table>
+    ?><br><br>
     </center>
 </body>
 </html>
