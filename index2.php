2f9af334d56e8f9b9fc877efc430636cf24a0573
MagenTa <t_o_tar_dexza@windowslive.com>
2019-02-08 19:02:03 +0700

    Update index2.php

--- index2.php
+++ index2.php
@@ -1,39 +1,18 @@
-<html>
-	<head>
-		<title>ข้อมูลการสมัครสมาชิกชมรมวิทยาศาสตร์</title>
-		<meta charset="UTF-8">
-
-		<link href="https://fonts.googleapis.com/css?family=Mali" rel="stylesheet">
-		<link href="form.css" rel="stylesheet">
-    	<style>
-      		body {
-        	font-family: 'Mali', cursive;
-        	font-size: 36px;
-      		}
-			fieldset {
-			 margin-bottom: 1em;
-			}    
-		</style>
-	</head>
-
-	<body background="bg1.jpg">
-			<?
-			if (isset($_POST["submit_form"])) {
+<?php
+	if (!isset($_POST["submit_form"])) {
 		
-				if(!checkdate($_POST["month"], $_POST["date"], $_POST["year"]-543)){
-		   		echo "<center><span style=\"color:red;background-color:#000000;\">วันเกิดไม่ถูกต้อง</span></center>";
-				break;
-				}elseif ($_POST["pass"]!= $_POST["repass"]){
-		     		echo "<center><span style=\"color:red;background-color:#000000;\">รหัสผ่านไม่ตรงกัน</span></center>";
-				break;
-		 		}elseif (!isset($_POST["agree"])){
-		    		echo "<center><span style=\"color:red;background-color:#000000;\">ไม่ได้ยอมรับข้อตกลง</span></center>";
-				break;
-				}else{
-		    		echo "<center><span style=\"color:red;background-color:#FFFFFF;\">ทำการสมัครเรียบร้อย</span></center>";
-				}
-			}
-			?>
-		<span>ชื่อ-สกุล : </span><?php echo $_POST["name"];?> <?php echo $_POST["name"];?>
-	</body>
-</html>
+		if(!checkdate($_POST["month"], $_POST["date"], $_POST["year"]-543)){
+		header('location: form.php');
+		echo "<center><span style=\"color:red;background-color:#000000;\">วันเกิดไม่ถูกต้อง</span></center>";
+		}elseif ($_POST["pass"]!= $_POST["repass"]){
+		header('location: form.php');
+		echo "<center><span style=\"color:red;background-color:#000000;\">รหัสผ่านไม่ตรงกัน</span></center>";
+		}elseif (!isset($_POST["agree"])){
+		header('location: form.php');
+		echo "<center><span style=\"color:red;background-color:#000000;\">ไม่ได้ยอมรับข้อตกลง</span></center>";
+		}else{
+		echo "<center><span style=\"color:red;background-color:#FFFFFF;\">ทำการสมัครเรียบร้อย</span></center>";
+		}
+	}
+?>
+
