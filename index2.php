705a21e8721a0f83a0e70bc864e97d4d006d8bf1
MagenTa <t_o_tar_dexza@windowslive.com>
2019-02-08 19:33:59 +0700

    Update index2.php

--- index2.php
+++ index2.php
@@ -3,13 +3,13 @@
 		
 		if(!checkdate($_POST["month"], $_POST["date"], $_POST["year"]-543)){
 			echo "<center><span style=\"color:red;background-color:#000000;\">วันเกิดไม่ถูกต้อง</span></center>";
-		include('location: form.php');
+		include('form.php');
 		}elseif ($_POST["pass"]!= $_POST["repass"]){
 			echo "<center><span style=\"color:red;background-color:#000000;\">วันเกิดไม่ถูกต้อง</span></center>";
-		include('location: form.php');
+		include('form.php');
 		}elseif (!isset($_POST["agree"])){
 			echo "<center><span style=\"color:red;background-color:#000000;\">วันเกิดไม่ถูกต้อง</span></center>";
-		include('location: form.php');
+		include('form.php');
 		}else{
 		echo "<center><span style=\"color:red;background-color:#FFFFFF;\">ทำการสมัครเรียบร้อย</span></center>";
 		}
