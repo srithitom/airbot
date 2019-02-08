9aa56b1c06ed99938cf1ff4716ff8d205728ad6d
MagenTa <t_o_tar_dexza@windowslive.com>
2019-02-08 18:07:44 +0700

    Update index2.php

--- index2.php
+++ index2.php
@@ -21,7 +21,7 @@
 	if (isset($_POST["submit_form"])) {
 
 		if(!checkdate($_POST["month"], $_POST["date"], $_POST["year"]-543)){
-   			echo "<span style="color:red;text-align:center;">วันเกิดไม่ถูกต้อง</span>";
+   			echo "<span style=\"color:red;text-align:center;\">วันเกิดไม่ถูกต้อง</span>";
 		}elseif ($_POST["pass"]!= $_POST["repass"]){
      		echo "รหัสผ่านไม่ตรงกัน";
  		}elseif (!isset($_POST["agree"])){
